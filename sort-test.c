508fa0032caff0766d2af8d8b1c29764c3f41618
by277 <by277@cornell.edu>
2019-09-20 16:22:21 -0400

    test_gdb_travis

--- sort-test.c
+++ sort-test.c
@@ -3,7 +3,7 @@
 
 void sort( int* x_ptr, int* y_ptr )
 {
-  if ( (*y_ptr) > (*x_ptr) ) {
+  if ( (*y_ptr) < (*x_ptr) ) {
     int temp = *x_ptr;
     *x_ptr   = *y_ptr;
     *y_ptr   = temp;
@@ -14,8 +14,13 @@ int main( void )
 {
   int a = 5;
   int b = 9;
+  int c = 10;
+  int d = 8;
   sort( &a, &b );
+  sort( &c, &d );
   UTST_ASSERT_INT_EQ( a, 5 );
   UTST_ASSERT_INT_EQ( b, 9 );
+  UTST_ASSERT_INT_EQ( c, 8 );
+  UTST_ASSERT_INT_EQ( d, 10 );
   return 0;
 }
